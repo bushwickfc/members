eb1f51121d7872903532f3432ecdc7984376fbe4
Pedro Moura <pmavfmoura@gmail.com>
2015-03-13 19:04:15 -0400

    fixed admin mailer test

--- test/mailers/admin_mailer_test.rb
+++ test/mailers/admin_mailer_test.rb
@@ -1,12 +1,12 @@
 require "test_helper"
 
 describe AdminMailer do
-  subject { create :member }
   
   it "sends a correct suspended email" do
-  	email = AdminMailer.suspended_email(subject)
-  	email_and_name = %("#{subject.first_name}" <#{subject.email}>)
-  	assert_equal email_and_name, email.to
-  	assert_equal 'BFC Membership Alert: You are currently suspended', subject.subject
+  	email = AdminMailer.suspended_email(@john)
+  	assert_equal @john.email, email.to[0]
+  	assert_equal 'BFC Membership Alert: You are currently suspended', email.subject
   end
+
+
 end
