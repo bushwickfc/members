afbb64f9d0a55125079c11539ea6b7b7ee5adf5a
Todd Morningstar <stratigos@users.noreply.github.com>
2015-08-13 12:42:22 -0400

    refs #16 Update TimeBank datetimepicker to disallow direct user input, and only allow input via datetimepicker;

--- app/assets/javascripts/time_banks.js
+++ app/assets/javascripts/time_banks.js
@@ -1,10 +1,14 @@
 /**
- * Applies the Bootstrap 3 Datetimepicker to the specified selector.
- *  Formatted to MySQL DATETIME to ease compatibility with dates in the past.
+ * Applies the Bootstrap 3 Datetimepicker to the specified selector. The input
+ *  is set to readonly to prevent erroneous user input, and valid dates from 
+ *  the datetimepicker calendar are allowed via the 'ignoreReadonly' parameter.
+ *  Input is formatted to MySQL DATETIME to ease compatibility with dates in
+ *  the past.
  * @return VOID
  */
 var dateWorkedDatetimepicker = function() {
-  $('#datetimepicker-dateworked').datetimepicker({format: 'YYYY-MM-DD'});
+  $('#datetimepicker-dateworked>input.form-control').prop("readonly", true);
+  $('#datetimepicker-dateworked').datetimepicker({format: 'YYYY-MM-DD', ignoreReadonly: true});
 };
 
 // Both doc.ready and doc.on(page:load) must be called due to Turbolinks. The
