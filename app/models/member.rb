5c738538514fa37923b9133e800750d951c16df4
Todd Morningstar <stratigos@users.noreply.github.com>
2015-11-08 22:49:23 -0500

    Refs #24 Return max Member.hours_owed at 8 hours;

--- app/models/member.rb
+++ app/models/member.rb
@@ -148,8 +148,10 @@ class Member < ActiveRecord::Base
     end.floor
   end
 
+  # Members should not go into debt beyond 8 hours, so regardless of status
+  #  or work history, members should never owe more than 8 hours.
   def hours_owed
-    work_in(:months) * monthly_hours
+    (hrs = work_in(:months) * monthly_hours) < 8.0 ? hrs : 8
   end
 
   def full_name
